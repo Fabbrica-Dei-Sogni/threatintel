5165140b0ff5574d2eee1ff5c03dca7dfacbc587
Alessandro Modica <alessandro.modica@gmail.com>
2026-01-08 11:24:55 +0000

    fix orientation pwa su tablet in verticale e orizzontale

--- frontend/dashboard/vite.config.ts
+++ frontend/dashboard/vite.config.ts
@@ -18,7 +18,7 @@ export default defineConfig({
                 display: 'standalone',
                 scope: '/',
                 start_url: '/',
-                orientation: 'portrait',
+                orientation: 'any',
                 icons: [
                     {
                         src: 'pwa-192x192.png',

779f37c2a546735c1bbcba5abb8ea1b7e963516a
Alessandro Modica <alessandro.modica@gmail.com>
2026-04-18 17:35:20 +0000

    feat: add port 4300 to CSP connectSrc and CORS origin whitelist

--- server.ts
+++ server.ts
@@ -35,7 +35,7 @@ app.use(helmet({
             styleSrc: ["'self'", "'unsafe-inline'"],
             scriptSrc: ["'self'", "'unsafe-inline'"],
             imgSrc: ["'self'", "data:", "https://validator.swagger.io"],
-            connectSrc: ["'self'", "https://alessandromodica.com", "http://82.112.255.186:5173"]
+            connectSrc: ["'self'", "https://alessandromodica.com", "http://82.112.255.186:5173", "http://82.112.255.186:4300", "http://localhost:4300"]
         },
     },
     hsts: {
@@ -47,7 +47,7 @@ app.use(helmet({
 
 // Middleware generali
 app.use(cors({
-    origin: ['http://localhost:5173', 'https://alessandromodica.com', 'http://82.112.255.186:5173'],
+    origin: ['http://localhost:5173', 'https://alessandromodica.com', 'http://82.112.255.186:5173', 'http://82.112.255.186:4300', 'http://localhost:4300'],
     credentials: true,
     exposedHeaders: ['Content-Disposition', 'Content-Length']
 }));

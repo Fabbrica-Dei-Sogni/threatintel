ceacc47dd25e99269ee3b8095139d7e8d3343a22
Alessandro Modica <alessandro.modica@gmail.com>
2026-04-30 13:30:27 +0000

    chore: add standard copyright and license headers to core API and endpoint files

--- core/apis/routes.ts
+++ core/apis/routes.ts
@@ -1,3 +1,11 @@
+/**
+ * ThreatIntel - Distributed Forensics Engine
+ * 
+ * Copyright (C) 2026 Alessandro Modica. All rights reserved.
+ * 
+ * Licensed under the Business Source License 1.1 (BSL-1.1).
+ * See root LICENSE.md for core engine licensing details.
+ */
 import express from 'express';
 import { RateLimitMiddleware } from '../rateLimitMiddleware';
 import { FakeLoginController } from '../controllers/FakeLoginController';
